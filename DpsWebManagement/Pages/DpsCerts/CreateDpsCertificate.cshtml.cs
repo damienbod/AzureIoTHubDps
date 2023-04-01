ffcfb78cfeff980049b3ab0f937f01f07789b564
damienbod <damien_bod@hotmail.com>
2023-04-01 19:22:10 +0200

    init test

--- DpsWebManagement/Pages/DpsCerts/CreateDpsCertificate.cshtml.cs
+++ DpsWebManagement/Pages/DpsCerts/CreateDpsCertificate.cshtml.cs
@@ -1,11 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.Xml;
 
 namespace DpsWebManagement.Pages.DpsCerts
 {
     public class CreateDpsCertificateModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public string? Message { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public IActionResult OnGet()
         {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                // Something failed. Redisplay the form.
+                return OnGet();
+            }
+
+            //var cert = GetCertificateWithPrivateKey();
+
+            Message = $"{Name}";
+
+            // Redisplay the form.
+            return OnGet();
         }
     }
 }
