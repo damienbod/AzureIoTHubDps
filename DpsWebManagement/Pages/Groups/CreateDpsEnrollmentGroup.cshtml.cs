948196ba120a1742add1fec6aad375994fae46d8
damienbod <damien_bod@hotmail.com>
2023-04-02 18:06:17 +0200

    Init groups setup create

--- DpsWebManagement/Pages/Groups/CreateDpsEnrollmentGroup.cshtml.cs
+++ DpsWebManagement/Pages/Groups/CreateDpsEnrollmentGroup.cshtml.cs
@@ -1,10 +1,62 @@
+using DpsWebManagement.Providers;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace DpsWebManagement.Pages.Groups;
 
 public class CreateDpsEnrollmentGroupModel : PageModel
 {
-    public void OnGet()
+    private readonly DpsCertificateProvider _dpsCertificateProvider;
+
+    [BindProperty]
+    public string? Message { get; set; }
+
+    [BindProperty]
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [BindProperty]
+    [Required]
+    public string DpsCertificate { get; set; } = string.Empty;
+
+    public List<SelectListItem> DpsCertificates { get; set; } = new List<SelectListItem>();
+
+    public CreateDpsEnrollmentGroupModel(DpsCertificateProvider dpsCertificateProvider)
     {
+        _dpsCertificateProvider = dpsCertificateProvider;
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        await GetSelectItems();
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            await GetSelectItems();
+            return await OnGetAsync();
+        }
+
+        //Message = $"{result.PublicPem}";
+        //Id = result.Id;
+
+        await GetSelectItems();
+        return await OnGetAsync();
+    }
+
+    private async Task GetSelectItems()
+    {
+        var result = await _dpsCertificateProvider.GetDpsCertificatesAsync();
+        DpsCertificates = result.Select(a =>
+            new SelectListItem
+            {
+                Value = a.Id.ToString(),
+                Text = a.Name
+            }).ToList();
     }
 }
--- DpsWebManagement/Program.cs
+++ DpsWebManagement/Program.cs
@@ -6,76 +6,75 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
-namespace DpsWebManagement
+namespace DpsWebManagement;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddDbContext<DpsDbContext>(options =>
+            options.UseSqlServer(connection)
+        );
+
+        builder.Services.AddCertificateManager();
+        builder.Services.AddTransient<DpsRegisterDevice>();
+        builder.Services.AddTransient<DpsEnrollmentGroup>();
+        builder.Services.AddTransient<DpsUpdateDevice>();
+
+        builder.Services.AddScoped<DpsCertificateProvider>();
+
+        builder.Services.AddDistributedMemoryCache();
+
+        builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+            .EnableTokenAcquisitionToCallDownstreamApi()
+            .AddDistributedTokenCaches();
+
+        builder.Services.AddAuthorization(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.FallbackPolicy = options.DefaultPolicy;
+        });
 
-            var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddRazorPages().AddMvcOptions(options =>
+        {
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+            options.Filters.Add(new AuthorizeFilter(policy));
+        }).AddMicrosoftIdentityUI();
 
-            builder.Services.AddDbContext<DpsDbContext>(options =>
-                options.UseSqlServer(connection)
-            );
+        builder.Services.AddControllers(options =>
+        {
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+            options.Filters.Add(new AuthorizeFilter(policy));
+        });
 
-            builder.Services.AddCertificateManager();
-            builder.Services.AddTransient<DpsRegisterDevice>();
-            builder.Services.AddTransient<DpsEnrollmentGroup>();
-            builder.Services.AddTransient<DpsUpdateDevice>();
+        var app = builder.Build();
 
-            builder.Services.AddScoped<DpsCertificateProvider>();
-
-            builder.Services.AddDistributedMemoryCache();
-
-            builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
-                .EnableTokenAcquisitionToCallDownstreamApi()
-                .AddDistributedTokenCaches();
-
-            builder.Services.AddAuthorization(options =>
-            {
-                options.FallbackPolicy = options.DefaultPolicy;
-            });
-
-            builder.Services.AddRazorPages().AddMvcOptions(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
-            }).AddMicrosoftIdentityUI();
-
-            builder.Services.AddControllers(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+        app.MapControllers();
+
+        app.Run();
     }
 }
