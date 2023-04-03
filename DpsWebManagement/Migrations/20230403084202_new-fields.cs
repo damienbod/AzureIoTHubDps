92eef00d0f13e9b5e3a8d5dcd76fbfa6756280b9
damienbod <damien_bod@hotmail.com>
2023-04-03 10:44:02 +0200

    update migrations

---
+++ DpsWebManagement/Migrations/20230403084202_new-fields.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DpsWebManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class newfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "DpsEnrollmentGroups",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "DpsEnrollmentGroups",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "DpsEnrollmentGroups");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "DpsEnrollmentGroups",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
