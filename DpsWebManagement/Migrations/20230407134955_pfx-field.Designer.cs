91cfe1c1a1b6fd75806bba2a4c4a3ca716b6bc49
damienbod <damien_bod@hotmail.com>
2023-04-07 15:50:46 +0200

    Update db

--- DpsWebManagement/Migrations/20230407134955_pfx-field.Designer.cs
+++ DpsWebManagement/Migrations/20230407134955_pfx-field.Designer.cs
@@ -3,6 +3,7 @@ using DpsWebManagement.Providers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DpsWebManagement.Migrations
 {
     [DbContext(typeof(DpsDbContext))]
-    partial class DpsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230407134955_pfx-field")]
+    partial class pfxfield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DpsWebManagement/Migrations/20230407134955_pfx-field.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DpsWebManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class pfxfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PemPrivateKey",
+                table: "DpsEnrollmentDevices");
+
+            migrationBuilder.RenameColumn(
+                name: "PemPublicKey",
+                table: "DpsEnrollmentDevices",
+                newName: "PathToPfx");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PathToPfx",
+                table: "DpsEnrollmentDevices",
+                newName: "PemPublicKey");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PemPrivateKey",
+                table: "DpsEnrollmentDevices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
