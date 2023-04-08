fb2616323c2cf7023883a7da763034db554c32fd
damienbod <damien_bod@hotmail.com>
2023-04-08 23:45:37 +0200

    code clean up

--- DpsWebManagement/Providers/FileProvider.cs
+++ DpsWebManagement/Providers/FileProvider.cs
@@ -1,31 +1,30 @@
 ﻿using System.Reflection;
 
-namespace DpsWebManagement.Providers
+namespace DpsWebManagement.Providers;
+
+public static class FileProvider
 {
-    public static class FileProvider
+    private static readonly bool _writeToDiskActive = false;
+    private static readonly bool _writePfxToDiskActive = true;
+
+    private static readonly string? _directory = Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location);
+    private static readonly string _pathToCerts = $"{_directory}\\..\\..\\..\\..\\Certs\\";
+
+    public static void WriteToDisk(string name, string pemPayload)
     {
-        static readonly string? _directory = Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location);
-        static readonly string _pathToCerts = $"{_directory}\\..\\..\\..\\..\\Certs\\";
-
-        private static bool _writeToDiskActive = false;
-        private static bool _writePfxToDiskActive = true;
-
-        public static void WriteToDisk(string name, string pemPayload)
+        if(_writeToDiskActive)
         {
-            if(_writeToDiskActive)
-            {
-                File.WriteAllText($"{_pathToCerts}{name}", pemPayload);
-            }
+            File.WriteAllText($"{_pathToCerts}{name}", pemPayload);
+        }
+    }
+
+    public static string WritePfxToDisk(string name, byte[] bytesPayload)
+    {
+        if (_writePfxToDiskActive)
+        {
+            File.WriteAllBytes($"{_pathToCerts}{name}", bytesPayload);
         }
 
-        public static string WritePfxToDisk(string name, byte[] bytesPayload)
-        {
-            if (_writePfxToDiskActive)
-            {
-                File.WriteAllBytes($"{_pathToCerts}{name}", bytesPayload);
-            }
-
-            return $"{_pathToCerts}{name}";
-        }
+        return $"{_pathToCerts}{name}";
     }
 }
